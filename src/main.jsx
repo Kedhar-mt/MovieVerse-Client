deb8eeb2f2653abc18e377506ee94ebac8ba77a9
Kedhareswar <129578151+Kedhar-mt@users.noreply.github.com>
2024-12-19 13:23:07 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import ApolloProviderWrapper from './ApolloClient.jsx';
 import { BrowserRouter } from 'react-router-dom'; 
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter> {/* Only wrap the App here */}
